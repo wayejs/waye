7bc6ab24604d7c20ccaf2f7585abd53154df6726
lynzz <lynzz168@gmail.com>
2019-07-19 13:05:37 +0800

    [build] 0.1.4

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue);
 }
 export default {
-  version: '0.1.3',
+  version: '0.1.4',
   install,
   BadgeStatus,
   ButtonConfirm,
